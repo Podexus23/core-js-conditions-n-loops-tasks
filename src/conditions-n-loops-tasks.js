04c83ecaada00ebfabd4fe5500fb5fd2e83b623b
Podexus23 <santon23@gmail.com>
2023-12-28 01:52:38 +0300

    feat: 10 - 9

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -232,8 +232,15 @@ function convertNumberToString(number) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  for (
+    let i = 0, j = str.length - 1;
+    i < str.length / 2 && j > str.length / 2;
+    i += 1, j -= 1
+  ) {
+    if (str[i] !== str[j]) return false;
+  }
+  return true;
 }
 
 /**
@@ -250,8 +257,11 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) return i;
+  }
+  return -1;
 }
 
 /**
@@ -269,8 +279,10 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const sNum = `${num}`;
+  const sDigit = `${digit}`;
+  return getIndexOf(sNum, sDigit) !== -1;
 }
 
 /**
