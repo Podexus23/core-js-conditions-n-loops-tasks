97352676965283ed2b6402b78f61fafba25bc2bc
Podexus23 <santon23@gmail.com>
2023-12-28 22:28:31 +0300

    feat: duck

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -298,8 +298,33 @@ function isContainNumber(num, digit) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  if (arr.length < 3) return -1;
+  let res = -1;
+
+  const left = new Array(arr.length - 1);
+  const right = new Array(arr.length - 1);
+  let leftCounter = 0;
+  let rightCounter = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    leftCounter += arr[i];
+    left[i] = leftCounter;
+  }
+
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    rightCounter += arr[i];
+    right[i] = rightCounter;
+  }
+
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    if (left[i] === right[i + 2]) {
+      res = i + 1;
+      break;
+    }
+  }
+
+  return res;
 }
 
 /**
